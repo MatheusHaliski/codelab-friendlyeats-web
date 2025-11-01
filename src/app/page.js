badb29c85d5e3972db1203f3ea528444a0135175
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-11-01 20:50:13 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -17,14 +17,17 @@ export default async function Home(props) {
   // Using seachParams which Next.js provides, allows the filtering to happen on the server-side, for example:
   // ?city=London&category=Indian&sort=Review
   const { firebaseServerApp } = await getAuthenticatedAppForUser();
-  const restaurants = await getRestaurants(
-    getFirestore(firebaseServerApp),
-    searchParams
-  const firestore = getFirestore(firebaseServerApp);
-  const restaurantsWithImages = await ensureRestaurantPhotos(
-    await getRestaurants(firestore, searchParams),
-    firestore
-  );
+const restaurants = await getRestaurants(
+  getFirestore(firebaseServerApp),
+  searchParams
+);
+
+const firestore = getFirestore(firebaseServerApp);
+const restaurantsWithImages = await ensureRestaurantPhotos(
+  restaurants,
+  firestore
+);
+
   return (
     <main className="main__home">
       <RestaurantListings

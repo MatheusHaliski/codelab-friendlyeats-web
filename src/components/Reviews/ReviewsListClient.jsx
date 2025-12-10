f961dba00b652c21902cfc1276333b8a0d847752
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-12-10 11:52:52 -0300

    Update ReviewsListClient.jsx

--- src/components/Reviews/ReviewsListClient.jsx
+++ src/components/Reviews/ReviewsListClient.jsx
@@ -16,27 +16,26 @@ export default function ReviewsListClient({
       setReviews(data);
     });
   }, [restaurantId]);
+
   return (
     <article>
-      <ul className="reviews">
-        {reviews.length > 0 ? (
-          <ul>
-            {reviews.map((review) => (
-              <Review
-                key={review.id}
-                rating={review.rating}
-                text={review.text}
-                timestamp={review.timestamp}
-              />
-            ))}
-          </ul>
-        ) : (
-          <p>
-            This restaurant has not been reviewed yet,{" "}
-            {!userId ? "first login and then" : ""} add your own review!
-          </p>
-        )}
-      </ul>
+      {reviews.length > 0 ? (
+        <ul className="reviews">
+          {reviews.map((review) => (
+            <Review
+              key={review.id}
+              rating={review.rating}
+              text={review.text}
+              timestamp={review.timestamp}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>
+          This restaurant has not been reviewed yet,{" "}
+          {!userId ? "first login and then" : ""} add your own review!
+        </p>
+      )}
     </article>
   );
 }

87844b4ff596b28c3606055d266122c138a36f51
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2026-01-06 15:13:51 -0300

    Move restaurant fetches to client after auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,11 @@
 import RestaurantListings from "@/src/components/RestaurantListings.jsx";
-import { getRestaurants } from "@/src/lib/firebase/firestore.js";
-import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp.js";
-import { getFirestore } from "firebase/firestore";
-import { moveFoodBackToRestaurants,moveLifestyleRestaurants,migrateLifestyleRestaurants } from "@/src/lib/firebase/migrations/moveLifestyleRestaurants";
-import { fixLifestyleTypeField } from "@/src/lib/firebase/migrations/fixLifestyleTypeField";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home(props) {
-  const searchParams = await props.searchParams;
-
-  const { firebaseServerApp } = await getAuthenticatedAppForUser();
-  const db = getFirestore(firebaseServerApp);
-
-  const normalizedFilters = {
-    ...searchParams,
-    type: searchParams.type || "food",
-  };
-
-  const restaurants = await getRestaurants(db, normalizedFilters);
-
+export default function Home({ searchParams }) {
   return (
     <main className="main__home">
-      <RestaurantListings
-        initialRestaurants={restaurants}
-        searchParams={searchParams}
-      />
+      <RestaurantListings searchParams={searchParams} />
     </main>
   );
 }
